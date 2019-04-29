1744f744a355b609e1f5b0e3eaa15a36444b0a84
jiao9206 <931642903@qq.com>
2019-04-29 10:20:44 +0800

    JFinal 事务控制

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -3,9 +3,11 @@ package controller;
 import java.util.List;
 import java.util.UUID;
 
+import com.jfinal.aop.Before;
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Controller;
 import com.jfinal.core.NotAction;
+import com.jfinal.plugin.activerecord.tx.Tx;
 import com.jfinal.ext.route.ControllerBind;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
@@ -34,15 +36,30 @@ public class HelloController extends Controller{
 	public void notAction() {
 		
 	}
+	@Before({Tx.class})
 	public void testDB() {
 		try {
 			List<Record> list=Db.find("select * from tb_test");
 			System.out.println(list);
-			Db.save("tb_test", new Record().set("id", UUID.randomUUID().toString().replaceAll("-", "")).set("name", "潘").set("age", 21).set("address", "山东济南"));
+			/**
+			 * Db.tx+lambda表达式进行事务控制，lambda表达式返回true,则提交事务，返回false则回滚事务
+			 */
+//			Db.tx(()->{
+//				Db.save("tb_test", new Record().set("id", UUID.randomUUID().toString().replaceAll("-", "")).set("name", "潘月").set("age", 21).set("address", "山东济南"));
+//				Db.save("tb_test", new Record().set("id", UUID.randomUUID().toString().replaceAll("-", "")).set("name", "潘小月").set("age", 21).set("address", "山东济南"));
+//				return true;
+//			});
+			Db.save("tb_test", new Record().set("id", UUID.randomUUID().toString().replaceAll("-", "")).set("name", "潘月").set("age", 21).set("address", "山东济南"));
+//			int i=1/0;
+			Db.save("tb_test", new Record().set("id", UUID.randomUUID().toString().replaceAll("-", "")).set("name", "潘小月").set("age", 21).set("address", "山东济南"));
 			renderText("SUCCESS!");
 		}catch(Exception e) {
 			e.printStackTrace();
 			renderText("Fail!");
+			/**
+			 * @Before({Tx.class})方式控制事务，发生异常的时候，必须向上抛出，Tx拦截器捕获异常后就会回滚事务
+			 */
+			throw e;
 		}
 		
 	}
